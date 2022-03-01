7b06c1f2852e2eddd5444423be4b26073ec7f035
shivkumar <shivprajapat232@gmail.com>
2022-03-01 11:18:44 +0530

    React TypeScript useReducer Hook Added

---
+++ src/components/state/Counter.tsx
@@ -0,0 +1,32 @@
+import { useReducer } from 'react'
+
+type CounterState = {
+    count: number
+}
+type CounterAction = {
+    type: string,
+    payload: number
+}
+const initialState = { count: 0 }
+
+function reducer(state: CounterState, action: CounterAction) {
+    switch (action.type) {
+        case 'increment':
+            return { count: state.count + action.payload }
+        case 'decrement':
+            return { count: state.count - action.payload }
+        default:
+            return state;
+    }
+}
+export default function Counter() {
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+    return (
+        <div>
+            <h2>Count: <b>{state.count}</b></h2>
+            <button onClick={() => dispatch({ type: "increment", payload: 10 })}>Increment 10</button>
+            <button onClick={() => dispatch({ type: "decrement", payload: 10 })}>Decrement 10</button>
+        </div>
+    )
+}
--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -4,20 +4,16 @@ type AuthUser = {
     email: string
 }
 export const User = () => {
-    const [user, setUser] = useState<AuthUser | null>(null)
+    const [user, setUser] = useState<AuthUser>({} as AuthUser)
     const handleLogin = () => {
         setUser({
             name: "Shiv",
             email: "shiv@gmail.com"
         })
     }
-    const handleLogout = () => {
-        setUser(null)
-    }
     return (
         <div>
             <button onClick={handleLogin}>Login</button>
-            <button onClick={handleLogout}>Logout</button>
             <h2>User name is {user?.name}</h2>
             <h2>User email is {user?.email}</h2>
         </div>
