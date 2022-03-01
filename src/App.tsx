a9e48c04e0c4c3dd6586b363ca25924a50551d26
shivkumar <shivprajapat232@gmail.com>
2022-03-01 09:27:02 +0530

    React TypeScript useState Hook Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Container from './components/Container';
+import { LoggedIn } from './components/LoggedIn';
 
 function App() {
 
   return (
     <div className="App">
       <Container styles={{ border: "1px solid red", padding: "1rem", display: "inline-blocks" }} />
+      <LoggedIn />
     </div>
   );
 }
